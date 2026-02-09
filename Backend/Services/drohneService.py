0c24a7752348cb6c8a8ebc2d94f56042ab56e02a
Simon <153181132+SimLan16@users.noreply.github.com>
2026-02-09 20:33:14 +0100

    Controller+Keyboardsteuerung finsished

--- Backend/Services/drohneService.py
+++ Backend/Services/drohneService.py
@@ -4,18 +4,20 @@ from robomaster import robot
 ep_drone = None
 
 def buildconnection(drone_ip: str) -> bool:
+    print("1")
     global ep_drone
     close()
-
+    print("2")
     try:
         robomaster.config.ROBOT_IP_STR = drone_ip
 
         ep_drone = robot.Drone()
         ok = ep_drone.initialize(conn_type="sta")
+        print("3")
         if not ok:
             close()
             return False
-
+        print("4")
         return True
 
     except Exception as e:
@@ -25,9 +27,11 @@ def buildconnection(drone_ip: str) -> bool:
 
 def close():
     global ep_drone
+    print("5")
     if ep_drone is not None:
         try:
             ep_drone.close()
+            print("6")
         except Exception:
             pass
         finally:
