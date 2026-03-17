4880278b3ff03f28442bb0040fda4e608dbc1524
LucaGruber1 <lucaegger08@gmx.at>
2026-03-17 18:17:26 +0100

    finished replay abspielen mit frontend

--- Frontend/src/app/services/drohne.service.ts
+++ Frontend/src/app/services/drohne.service.ts
@@ -31,7 +31,7 @@ export class DroneService {
 
 
   playSavedFlight(flightName: string): Observable<any> {
-    return this.http.post(`${this.baseUrl}/play`, { name: flightName });
+    return this.http.post(`${this.baseUrl}/play-flight`, { name: flightName });
   }
 
   saveFlight(payload: { ip: string, courseName: string }): Observable<any> {
