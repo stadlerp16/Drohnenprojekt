5ee0bce0779ffb8e6f14d4fe458f17f1b98205b0
LucaGruber1 <lucaegger08@gmx.at>
2026-06-10 09:38:15 +0200

    Merge branch 'Mario' of https://github.com/stadlerp16/Drohnenprojekt into Mario
    # Conflicts:
    #	Backend/.idea/workspace.xml

--- Frontend/src/app/services/drohne.service.ts
+++ Frontend/src/app/services/drohne.service.ts
@@ -7,9 +7,9 @@ import {VideoItem} from '../../home/home';
 
 @Injectable({ providedIn: 'root' })
 export class DroneService {
-  private baseUrl = 'http://192.168.121.71:8000/drone';
-  private videoUrl = 'http://192.168.121.71:8000/video';
-  private wsUrl = 'ws://192.168.121.71:8000/drone/telemetrie';
+  private baseUrl = 'http://localhost:8000/drone';
+  private videoUrl = 'http://localhost:8000/video';
+  private wsUrl = 'ws://localhost:8000/drone/telemetrie';
 
   // Zentraler Status
   isConnected = false;
@@ -107,7 +107,7 @@ export class DroneService {
   }
 
   getVideoStreamSocket(): WebSocket {
-    return new WebSocket('ws://192.168.121.71:8000/video/getlivestream');
+    return new WebSocket('ws://localhost:8000/video/getlivestream');
   }
 
   // --- VIDEO RECORDING ---
